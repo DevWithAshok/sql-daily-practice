1fe89461919558ed87792b085e6dc4cf500d5696
Ashok Kumar Reddy Chinnavula <ashok052606@gmail.com>
2026-04-08 14:32:15 +0530

    Day 19 - Added UNION operator to combine multiple query results

---
+++ Day 19 - UNION Operator/union_operator.sql
@@ -0,0 +1,20 @@
+SELECT
+  DISTINCT name AS name
+FROM
+  player
+WHERE
+  UPPER(played_for_team) = "SRH"
+UNION
+SELECT
+  DISTINCT name AS name
+FROM
+  player
+WHERE
+  UPPER(played_for_team) = "CSK"
+UNION
+SELECT
+  DISTINCT name AS name
+FROM
+  player
+WHERE
+  UPPER(played_for_team) = "MI";
