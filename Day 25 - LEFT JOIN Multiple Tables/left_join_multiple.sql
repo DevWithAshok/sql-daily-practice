49d49c0ebdb4f7d37f5f4bf444cf0a410e3b09cc
Ashok Kumar Reddy Chinnavula <ashok052606@gmail.com>
2026-04-15 21:49:15 +0530

    Day 25 - Added LEFT JOIN with multiple tables query

---
+++ Day 25 - LEFT JOIN Multiple Tables/left_join_multiple.sql
@@ -0,0 +1,13 @@
+SELECT
+  student.id AS student_id,
+  student.full_name AS student_name,
+  course.id AS course_id,
+  course.name AS course_name,
+  score,
+  enrollment_date
+FROM
+  (
+    student
+    LEFT JOIN student_course ON student.id = student_course.student_id
+  ) AS T
+  LEFT JOIN course ON course.id = T.course_id;
